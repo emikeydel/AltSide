73c0c137ec3b8b2f76c89f20fb7e3e044386230a
Mikey <emikey@gmail.com>
2026-03-30 10:38:22 -0400

    Initial Commit

---
+++ AltSide/AltSide.swift
@@ -0,0 +1,9 @@
+//
+//  AltSide.swift
+//  AltSide
+//
+//  Created by Mikey del Rosario on 3/30/26.
+//
+
+import Foundation
+
---
+++ AltSideTests/AltSideTests.swift
@@ -0,0 +1,19 @@
+//
+//  AltSideTests.swift
+//  AltSideTests
+//
+//  Created by Mikey del Rosario on 3/30/26.
+//
+
+import Testing
+@testable import AltSide
+
+struct AltSideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
